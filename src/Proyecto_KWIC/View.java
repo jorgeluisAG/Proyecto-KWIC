9d65899a4fed01c225310c328c7ebc9b201cb7f0
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2021-08-17 14:52:29 -0400

    Se agrega el Proyecto

---
+++ src/Proyecto_KWIC/View.java
@@ -0,0 +1,13 @@
+package Proyecto_KWIC;
+
+import java.awt.EventQueue;
+
+import View.VentanaPrincipal;
+
+public class View {
+
+	public static void main(String[] args) {
+		VentanaPrincipal miVentana = new VentanaPrincipal();
+		miVentana.setVible(true);
+	}
+}
