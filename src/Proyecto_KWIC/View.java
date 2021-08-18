69d95a986b671ec5719dafd4e21628dcc0e1b820
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2021-08-17 22:16:45 -0400

    Se agrego subida de la lista de los libros

--- src/Proyecto_KWIC/View.java
+++ src/Proyecto_KWIC/View.java
@@ -6,20 +6,31 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JScrollBar;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 
 public class View {
 
 	private JFrame frmKwic;
 	private JTextField textField;
+	private JTextArea textArea;
+	private JTextArea textArea1;
+	private JScrollPane scrollPane;
+	private JScrollPane scrollPane1;
+	private JButton btnBuscar;
 
 	/**
 	 * Launch the application.
@@ -54,40 +65,65 @@ public class View {
 		frmKwic.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmKwic.getContentPane().setLayout(null);
 
-		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.setFont(new Font("Tahoma", Font.BOLD, 14));
-		btnBuscar.addActionListener(new ActionListener() {
+		JButton btnSubirArchivo = new JButton("Subir Archivo");
+		btnSubirArchivo.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnSubirArchivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				fc.showOpenDialog(null);
+				File archivo = fc.getSelectedFile();
+				try {
+					FileReader fr = new FileReader(archivo);
+					BufferedReader br = new BufferedReader(fr);
+					String texto="";
+					String linea="";
+					while(((linea=br.readLine())!=null)) {
+						texto+=linea+"\n";
+					}
+					textArea.setText(texto);
+					//JOptionPane.showMessageDialog(null, "Archivo Leido Correctamente");
+				}catch(Exception e1) {
+					
+				}
 			}
 		});
-		btnBuscar.setBounds(102, 333, 119, 35);
-		frmKwic.getContentPane().add(btnBuscar);
+		btnSubirArchivo.setBounds(102, 382, 138, 35);
+		frmKwic.getContentPane().add(btnSubirArchivo);
 
 		textField = new JTextField();
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		textField.setBounds(266, 335, 534, 35);
+		textField.setBounds(250, 382, 421, 35);
 		frmKwic.getContentPane().add(textField);
 		textField.setColumns(10);
-
-		JScrollPane scrollPane1 = new JScrollPane();
-		scrollPane1.setBounds(449, 111, 352, 191);
-		frmKwic.getContentPane().add(scrollPane1);
-
-		JScrollPane scrollPane2 = new JScrollPane();
-		scrollPane2.setBounds(449, 397, 351, 234);
-		frmKwic.getContentPane().add(scrollPane2);
 
 		JLabel titulo = new JLabel("Key Word In Context");
 		titulo.setFont(new Font("Tahoma", Font.BOLD, 30));
 		titulo.setBounds(279, 41, 369, 35);
 		frmKwic.getContentPane().add(titulo);
+		
 
-		JPanel panel = new JPanel();
-		panel.setBounds(102, 111, 337, 191);
-		frmKwic.getContentPane().add(panel);
-
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(102, 397, 337, 234);
-		frmKwic.getContentPane().add(panel_1);
+		
+		textArea = new JTextArea();
+		textArea.setEnabled(true);
+		textArea.setEditable(false);
+		scrollPane = new JScrollPane(textArea);
+		scrollPane.setBounds(102, 111, 698, 231);
+		frmKwic.getContentPane().add(scrollPane);
+		
+		textArea1 = new JTextArea();
+		textArea1.setEnabled(true);
+		textArea1.setEditable(false);
+		scrollPane1 = new JScrollPane(textArea1);
+		scrollPane1.setBounds(102, 446, 698, 191);
+		frmKwic.getContentPane().add(scrollPane1);
+		
+		btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnBuscar.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnBuscar.setBounds(681, 382, 119, 35);
+		frmKwic.getContentPane().add(btnBuscar);
 	}
 }
